85e91eaca034a00c8f24d262767e5efaa8ba0ab4
zaru@sakuraba <zarutofu@gmail.com>
2016-04-30 22:36:08 +0900

    first commit

---
+++ NotifyHub/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  NotifyHub
+//
+//  Created by hiro on 2016/04/30.
+//  Copyright © 2016年 zaru. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
