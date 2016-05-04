fc0093af13cb8ee51041c1ae6362e4763dd9257f
zaru@sakuraba <zarutofu@gmail.com>
2016-05-04 23:50:35 +0900

    view NSTableView

--- NotifyHub/NotifyHubViewController.swift
+++ NotifyHub/NotifyHubViewController.swift
@@ -8,18 +8,42 @@
 
 import Cocoa
 
-class NotifyHubViewController: NSViewController {
+class NotifyHubViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
 
-    @IBOutlet weak var tv: NSScrollView!
+    
+    @IBOutlet weak var tableView: NSTableView!
+    var lists: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         
+        tableView.setDelegate(self)
+        tableView.setDataSource(self)
+        
         let notificationModel = NotificationModel()
         notificationModel.fetchLists({ json in
-            print(json)
+            self.lists = json
+            self.tableView.reloadData()
         })
+    }
+    
+    func numberOfRowsInTableView(aTableView: NSTableView) -> Int
+    {
+        return self.lists.count
+    }
+    func tableView(aTableView: NSTableView,
+                   objectValueForTableColumn aTableColumn: NSTableColumn?,
+                                             row rowIndex: Int) -> AnyObject?
+    {
+        let columnName = aTableColumn?.identifier
+        print(columnName)
+        
+        if columnName == "Message" {
+            print(self.lists[rowIndex])
+            return self.lists[rowIndex]
+        }
+        return ""              
     }
     
 }
